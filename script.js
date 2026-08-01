d5a4c84da6eab0ae689e9eee90e4e05a908c09ee
Geervani S <grieshmas13@gmail.com>
2026-08-01 21:05:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ const year = new Date().getFullYear();
 const footer = document.querySelector("footer p");
 
 if (footer) {
-    footer.innerHTML = `© ${year} Your Name. All Rights Reserved.`;
+    footer.innerHTML = `© ${year} Geervani S. All Rights Reserved.`;
 }
